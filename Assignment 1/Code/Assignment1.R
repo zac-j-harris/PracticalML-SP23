0b231fd770009ab5420b8bba77614995847d5d84
zac-j-harris <54678510+zac-j-harris@users.noreply.github.com>
2023-02-24 17:32:36 -0700

    Update Assignment1.R

--- Assignment 1/Code/Assignment1.R
+++ Assignment 1/Code/Assignment1.R
@@ -1,28 +1,29 @@
 # Zac Harris
 # 
 # Set and check working Directory
-setwd("~/Desktop/  /SP 23/Practical ML/")
+setwd("~/Documents/Github/PracticalML-SP23/Assignment 1/")
 getwd()
 
-
 # Import dataset
-white_wine = read.csv("./wine_quality/winequality-white.csv", sep=";")
+white_wine = read.csv("./Data/wine_quality/winequality-white.csv", sep=";")
 
 # Generate pairs graph
-# pairs(white_wine)
+pairs(white_wine)
 
-
-# Grab dimensions
+# Print the dimensions
 dim(white_wine)
 
-# Generate 
+# Generate, function sourced from: https://www.statology.org/remove-outliers-r/
 z_scores <- as.data.frame(sapply(white_wine, function(ww) (abs(ww-mean(ww))/sd(ww))))
 
+# Remove all points >5 std away from mean.
 no_outliers <- white_wine[!rowSums(z_scores>5), ]
+
+# Print the new dimensions
 dim(no_outliers)
 
-# Generate pairs graph
-# pairs(no_outliers)
+# Generate new pairs graph
+pairs(no_outliers)
 
 # Save the new csv
-write.csv(no_outliers, "./wine_quality/fixed-winequality-whites.csv")
+write.csv(no_outliers, "./Data/wine_quality/fixed-winequality-whites.csv")
